7585e1865c7434157c0555b89433171e25bc662b
Lugia21 <kevinadeva@ymail.com>
2018-11-08 07:26:09 +0800

    Store to Github

---
+++ app/src/main/java/com/a4ita/adeva/adverbproject/SplashScreen.java
@@ -0,0 +1,13 @@
+package com.a4ita.adeva.adverbproject;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+    }
+}
