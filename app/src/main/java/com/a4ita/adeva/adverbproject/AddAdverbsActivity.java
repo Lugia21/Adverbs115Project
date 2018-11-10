99164876cf311f0a3a3f1dea7db9e5e469f802d6
Lugia21 <kevinadeva@ymail.com>
2018-11-10 21:37:52 +0800

    Navigation Bar buttons link to Activities working

--- app/src/main/java/com/a4ita/adeva/adverbproject/AddAdverbsActivity.java
+++ app/src/main/java/com/a4ita/adeva/adverbproject/AddAdverbsActivity.java
@@ -1,36 +1,31 @@
 package com.a4ita.adeva.adverbproject;
 
-
 import android.content.Intent;
 import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
 import android.support.design.widget.NavigationView;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
-import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class AddAdverbsActivity extends AppCompatActivity {
 
     private DrawerLayout dl;
-    ActionBarDrawerToggle t;
-    NavigationView nv;
-    Intent intent;
+    private ActionBarDrawerToggle t;
+    private NavigationView nv;
+    private Intent intent;
 
     @Override
-    @Nullable
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.add_adverbs_activity);
 
-        dl = (DrawerLayout) findViewById(R.id.activity_main);
+        dl = (DrawerLayout) findViewById(R.id.add_adverbs_activity);
         t = new ActionBarDrawerToggle(this, dl, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
 
         dl.addDrawerListener(t);
         t.syncState();
-
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         nv = (NavigationView) findViewById(R.id.nv);
@@ -42,21 +37,21 @@ public class MainActivity extends AppCompatActivity {
                 dl.closeDrawers();
                 switch (id) {
                     case R.id.learn:
-                        intent = new Intent(MainActivity.this, MainActivity.class);
+                        intent = new Intent(AddAdverbsActivity.this, MainActivity.class);
                         startActivity(intent);
                         break;
                     case R.id.test:
-                        intent = new Intent(MainActivity.this, TestActivity.class);
+                        intent = new Intent(AddAdverbsActivity.this, TestActivity.class);
                         startActivity(intent);
                         break;
                     case R.id.addAdverbs:
-                        intent = new Intent(MainActivity.this, AddAdverbsActivity.class);
+                        intent = new Intent(AddAdverbsActivity.this, AddAdverbsActivity.class);
                         startActivity(intent);
                         break;
                     default:
                         return true;
                 }
-            return true;
+                return true;
             }
         });
     }
@@ -64,9 +59,9 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
 
-        if(t.onOptionsItemSelected(item)) {
+        if(t.onOptionsItemSelected(item))
             return true;
-        }
+
         return super.onOptionsItemSelected(item);
     }
 }
