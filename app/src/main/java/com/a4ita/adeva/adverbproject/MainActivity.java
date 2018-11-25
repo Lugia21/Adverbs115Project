3b3e3d2822cf94249eb52c85f493b323a839d21f
Lugia21 <kevinadeva@ymail.com>
2018-11-25 16:55:59 +0800

    ALL MODULES WORKING, Only design is needed

--- app/src/main/java/com/a4ita/adeva/adverbproject/MainActivity.java
+++ app/src/main/java/com/a4ita/adeva/adverbproject/MainActivity.java
@@ -10,7 +10,10 @@ import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
+import com.github.aakira.expandablelayout.ExpandableRelativeLayout;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,6 +21,7 @@ public class MainActivity extends AppCompatActivity {
     ActionBarDrawerToggle t;
     NavigationView nv;
     Intent intent;
+    ExpandableRelativeLayout expandableLayout1, expandableLayout2, expandableLayout3, expandableLayout4, expandableLayout5;
 
     @Override
     @Nullable
@@ -58,7 +62,11 @@ public class MainActivity extends AppCompatActivity {
                 }
             return true;
             }
-        });
+            });
+
+
+
+
     }
 
     @Override
@@ -69,4 +77,26 @@ public class MainActivity extends AppCompatActivity {
         }
         return super.onOptionsItemSelected(item);
     }
+
+    public void expandableButton1(View view) {
+        expandableLayout1 = (ExpandableRelativeLayout) findViewById(R.id.expandableLayout1);
+        expandableLayout1.toggle(); // toggle expand and collapse
+    }
+
+    public void expandableButton2(View view) {
+        expandableLayout2 = (ExpandableRelativeLayout) findViewById(R.id.expandableLayout2);
+        expandableLayout2.toggle(); // toggle expand and collapse
+    }
+
+    public void expandableButton3(View view) {
+        expandableLayout3 = (ExpandableRelativeLayout) findViewById(R.id.expandableLayout3);
+        expandableLayout3.toggle(); // toggle expand and collapse
+    }
+
+    public void expandableButton4(View view) {
+        expandableLayout4 = (ExpandableRelativeLayout) findViewById(R.id.expandableLayout4);
+        expandableLayout4.toggle(); // toggle expand and collapse
+    }
+
+
 }
