0d7f78d82f41a449376500c01c47f50de5ada057
Lugia21 <kevinadeva@ymail.com>
2018-12-11 22:43:45 +0800

    Finalized Design Colors + ICON and App Title

--- app/src/main/java/com/a4ita/adeva/adverbproject/SplashActivity.java
+++ app/src/main/java/com/a4ita/adeva/adverbproject/SplashActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Window;
 
 
 public class SplashActivity extends AppCompatActivity {
@@ -14,6 +15,7 @@ public class SplashActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getSupportActionBar().hide();
         setContentView(R.layout.splash_screen);
 
         handler=new Handler();
